6b571aa2598b3994a6df5b7a7027d371a9516fc8
unknown <kimdongju3331@gmail.com>
2021-05-21 17:31:24 +0900

    list를 이용한 stock 예제

--- list/Stock.py
+++ list/Stock.py
@@ -1,22 +1,38 @@
 class Stock(object):
-    def __init__(self, stock_name, stock_code):
+    def __init__(self, stock_name, code):
         self.stock_name = stock_name
-        self.stock_code = stock_code
+        self.code = code
 
     def get_stock(self):
-        return f'종목명 {self.stock_name}, 종목코드 {self.stock_code}'
+        return f'종목명 {self.stock_name}, 종목코드 {self.code}'
+
+    @staticmethod
+    def modify_element(ls, code):
+        for i, j in enumerate(ls):
+            if j.code == code:
+                return ls[i]
 
     @staticmethod
     def main():
+        ls = []
         while True:
-            n = input('프로그램종료0, 종목명과 종목코드 입력1, 종목명과 종목코드 출력2')
-            print(n)
-            if n == '0':
+            menu = input('0.Exit 1.Create 2.Read 3.Update 4.Delete')
+            if menu == '0':
                 break
-            elif n == '1':
-                s = Stock(input('종목명'), input('종목코드'))
-            elif n == '2':
-                print(s.get_stock())
+            elif menu == '1':
+                ls.append(Stock(input('종목명'), input('종목코드')))
+            elif menu == '2':
+                for i in ls:
+                    print(i.get_stock())
+            elif menu == '3':
+                edit_code = input('수정할 종목코드')
+                stock = Stock.modify_element(ls, edit_code)
+                if stock:
+                    stock.stock_name = (input('수정할 종목이름'))
+            elif menu == '4':
+                stock = Stock.modify_element(ls, input('삭제할 종목코드'))
+                if stock:
+                    ls.remove(stock)
             else:
                 print('다시 입력하세요')
                 continue
